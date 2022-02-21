6cdb2a221d9cdd07f3da7a3ab62453a853fedd31
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-02-21 12:26:51 +0200

    add test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -9,9 +9,7 @@ const request = supertest(server.app);
 describe("testing API server", () => {
   it("testing /", async () => {
     const response = await request.get("/");
-    // console.log(response);
     expect(response.text).toEqual("home page");
-    // expect(response.status).toEqual(200);
   });
 
   it("test /data", async () => {
